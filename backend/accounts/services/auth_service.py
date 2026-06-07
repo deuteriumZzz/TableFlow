02e0ac4887596ade7a0d5f0fcaf128d96f85a075
Dmitry Vologdin <deuteriumzzz@gmail.com>
2026-06-07 12:24:16 +0800

    add auth service

---
+++ backend/accounts/services/auth_service.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import authenticate
+from datetime import timezone
+
+from django.contrib.auth.hashers import make_password
+
+from backend.accounts.models import User
+
+
+class AuthService:
+    @staticmethod
+    def authenticate_user(username, password):
+        user = authenticate(username=username, password=password)
+        if user:
+            user.last_login_pos = timezone.now()
+            user.save()
+        return user
+
+    @staticmethod
+    def create_user(data):
+        user = User(
+            username=data["username"],
+            email=data["email"],
+            password=make_password(data["password"]),
+            first_name=data.get("first_name", ""),
+            last_name=data.get("last_name", ""),
+            role=data.get("role", "cashier"),
+        )
+        user.save()
+        return user
